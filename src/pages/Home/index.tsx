c7834e8ea2d5debd88a697efafb538b6181299b9
VictorFujiyama <victorfujiyama@gmail.com>
2024-06-12 23:38:12 -0300

    third commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ export function Home() {
           />
 
           <datalist id="task-suggestions">
-            <option value="teste final" />
+            <option value="Proejto1" />
             <option value="Projeto2" />
             <option value="Projeto3" />
             <option value="Banana" />
