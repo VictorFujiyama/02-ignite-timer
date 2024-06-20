b08bd9715bc4d9ad700de98f8ecc8f88963f3cb9
VictorFujiyama <victorfujiyama@gmail.com>
2024-06-20 16:51:55 -0300

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-
-import { defaultTheme } from './styles/themes/default'
-import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
+
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
@@ -11,9 +11,8 @@ export function App() {
       <BrowserRouter>
         <Router />
       </BrowserRouter>
+
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
-export default App
